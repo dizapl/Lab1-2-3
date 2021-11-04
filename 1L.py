c377e10c2e8e46fcba81be62e835a7fbeba3710a
Заплетнюк Діана <90466495+dizapl@users.noreply.github.com>
2021-11-04 22:42:15 +0200

    Add files via upload

---
+++ 1L.py
@@ -0,0 +1,10 @@
+
+import random
+
+a = ['Sasha', 'Zhenya', 'Artem', 'Max', 'Bohdan', 'Vova', 'Murat', 'Semi']
+b = ['Tonya', 'Dasha', 'Diana', 'Alina', 'Sofia', 'Tina']
+c = ['friends', 'enemies', 'relatives', 'neighborhood']
+S1 =random.choice(a) 
+S2 =random.choice(b)
+S3 =random.choice(c)
+print(S1 +' and '+ S2 +' are '+S3)
